8bace3deda9879e5e1921738d8479f126df3b8a6
pragati <itispragativerma@gmail.com>
2020-05-10 19:00:21 +0530

    first class component

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-function Greet(){
-  return <h1>Hello Pragati</h1>
-}
+// function Greet(){
+//   return <h1>Hello Pragati</h1>
+// }
+
+const Greet = ()=> <h1> Hello Pragati</h1>
 
 export default Greet;
