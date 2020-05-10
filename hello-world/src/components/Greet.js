161e70920625077ab6ebc4dfdee1439ede7eafa1
pragati <itispragativerma@gmail.com>
2020-05-10 22:56:33 +0530

    introduction to props

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -4,6 +4,9 @@ import React from 'react';
 //   return <h1>Hello Pragati</h1>
 // }
 
-const Greet = ()=> <h1> Hello Pragati</h1>
+const Greet = (props)=> {
+
+return <h1> Hello {props.name}</h1>
+}
 
 export default Greet;
