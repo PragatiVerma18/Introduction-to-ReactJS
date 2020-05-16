eaa35e30cbe0903cb0570cb8032a1051a5ccc0fa
pragati <itispragativerma@gmail.com>
2020-05-16 20:26:11 +0530

    binding event handlers

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -8,26 +8,12 @@ import Message from './components/Message'
 import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
+import EventBind from './components/EventBind'
 class App extends Component {
   render(){
   return (
     <div className="App">
-      {/* <FunctionClick></FunctionClick> */}
-
-      <ClassClick></ClassClick>
-
-      {/* <Counter/> */}
-      {/* <Message/> */}
-
-       {/* <Greet name="Bruce" heroName="Batman"><p>This is children props</p></Greet> 
-       <Greet name="Clark" heroName="Superman">
-         <button> Action </button>
-         </Greet> 
-       <Greet name="Diana" heroName="Wonder Woman"/>  */}
-      {/* <Welcome name="Bruce" heroName="Batman"/> 
-      <Welcome name="Clark" heroName="Superman"/> 
-      <Welcome name="Diana" heroName="Wonder Woman"/>  */}
-      {/* <Hello/> */}
+      <EventBind></EventBind>
     </div>
   );
 }
