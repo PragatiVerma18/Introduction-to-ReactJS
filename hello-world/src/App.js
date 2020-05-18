6702bb74c372f320abce71fd2162fd0193b851e6
pragati <itispragativerma@gmail.com>
2020-05-18 11:35:29 +0530

    conditional rendering

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import ParentComponent from './components/ParentComponent'
+import UserGreeting from './components/UserGreeting'
 class App extends Component {
   render(){
   return (
     <div className="App">
-      <ParentComponent></ParentComponent>
+      <UserGreeting></UserGreeting>
     </div>
   );
 }
