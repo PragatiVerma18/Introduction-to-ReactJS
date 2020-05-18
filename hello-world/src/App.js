cb6ec347f4ff951bdea30239576f9aa4a04b5552
pragati <itispragativerma@gmail.com>
2020-05-19 01:19:26 +0530

    styling and css basics

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import NameList from './components/NameList';
+import StyleSheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyle.css';
+import styles from './appStyles.module.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<NameList></NameList>
+				{/* <StyleSheet primary={true} />  */}
+				{/* <Inline /> */}
+				<h1 className='error'>Error</h1>
+				<h1 className={styles.success}>Sucess</h1>
 			</div>
 		);
 	}
