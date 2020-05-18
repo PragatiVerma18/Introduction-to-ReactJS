3f841b9c4fc5e8b82174039b0d46db36c5640a3d
pragati <itispragativerma@gmail.com>
2020-05-19 00:38:00 +0530

    index as keys

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import NameList from './components/NameList'
+import NameList from './components/NameList';
 
 class App extends Component {
-  render(){
-  return (
-    <div className="App">
-      <NameList></NameList>
-    </div>
-  );
-}
+	render() {
+		return (
+			<div className='App'>
+				<NameList></NameList>
+			</div>
+		);
+	}
 }
 export default App;
--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -1,39 +1,40 @@
-import React from 'react'
-import Person from './Person'
+import React from 'react';
+import Person from './Person';
+
 function NameList() {
-  const names = ['Bruce', 'Clark', 'Diana']
-  const persons = [
-    {
-      id: 1,
-      name: 'Bruce',
-      age: 28,
-      skill: 'Vue'
-    },
-    {
-      id: 2,
-      name: 'Clark',
-      age: 30,
-      skill: 'React'
-    },
-    {
-      id: 3,
-      name: 'Diana',
-      age: 25,
-      skill: 'Angular'
-    },
-    {
-      id: 4,
-      name: 'David',
-      age: 40,
-      skill: 'Django'
-    }
-  ]
-  // const nameList = names.map(name => <h2>{name}</h2>)
-  // return (
-  //   <div>{nameList}</div>
-  // )
-  const personList = persons.map(person => <Person key={person.id} person={person}/>)
-return <div>{personList}</div>
+	const names = ['Bruce', 'Clark', 'Diana'];
+	const persons = [
+		{
+			id: 1,
+			name: 'Bruce',
+			age: 28,
+			skill: 'Vue',
+		},
+		{
+			id: 2,
+			name: 'Clark',
+			age: 30,
+			skill: 'React',
+		},
+		{
+			id: 3,
+			name: 'Diana',
+			age: 25,
+			skill: 'Angular',
+		},
+		{
+			id: 4,
+			name: 'David',
+			age: 40,
+			skill: 'Django',
+		},
+	];
+	const nameList = names.map((name, index) => (
+		<h2 key={index}>
+			{index} {name}
+		</h2>
+	));
+	return <div> {nameList} </div>;
 }
 
-export default NameList
+export default NameList;
