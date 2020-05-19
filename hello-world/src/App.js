0919ab9875edc9463ae873885319427a0acb1920
pragati <itispragativerma@gmail.com>
2020-05-19 14:09:50 +0530

    form handling in react

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,19 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import StyleSheet from './components/Stylesheet';
-import Inline from './components/Inline';
-import './appStyle.css';
-import styles from './appStyles.module.css';
+import Form from './components/Form';
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				{/* <StyleSheet primary={true} />  */}
-				{/* <Inline /> */}
-				<h1 className='error'>Error</h1>
-				<h1 className={styles.success}>Sucess</h1>
+				<Form></Form>
 			</div>
 		);
 	}
