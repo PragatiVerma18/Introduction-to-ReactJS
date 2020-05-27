2a8f9a95b2f5015e485d16acb1661ee2a084259f
pragati <itispragativerma@gmail.com>
2020-05-27 18:44:49 +0530

    Refs

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,16 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import FragmentDemo from './components/FragementDemo';
-import Table from './components/Table';
+import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<Table />
-				{/* <FragmentDemo /> */}
+				<RefsDemo />
 			</div>
 		);
 	}
---
+++ hello-world/src/components/RegComp.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class RegComp extends Component {
+	render() {
+		console.log('Regular Component Render');
+		return <div>Regular Component</div>;
+	}
+}
+
+export default RegComp;
