c4ceebcc1c5e0344ee0b3e3b679bffe886ff4645
pragati <itispragativerma@gmail.com>
2020-05-27 19:13:10 +0530

    forwarding refs

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,15 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ParentComp from './components/ParentComp';
-import RefsDemo from './components/RefsDemo';
-import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<FocusInput />
+				<FRParentInput />
 			</div>
 		);
 	}
