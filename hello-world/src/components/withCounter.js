76539cdc65c4a3e8fcc6f5de18d14136b5ac96d6
pragati <itispragativerma@gmail.com>
2020-05-28 22:01:42 +0530

    hoc 3

--- hello-world/src/components/withCounter.js
+++ hello-world/src/components/withCounter.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
-const UpdatedComponent = (OriginalComponent) => {
-	class NewComponent extends React.Component {
+const withCounter = (WrappedComponent, incrementNumber) => {
+	class withCounter extends React.Component {
+		constructor(props) {
+			super(props);
+
+			this.state = {
+				count: 0,
+			};
+		}
+
+		incrementCount = () => {
+			this.setState((prevState) => {
+				return { count: prevState.count + incrementNumber };
+			});
+		};
 		render() {
-			return <OriginalComponent name='Vishwas' />;
+			return (
+				<WrappedComponent
+					count={this.state.count}
+					incrementCount={this.incrementCount}
+					{...this.props}
+				/>
+			);
 		}
 	}
-	return NewComponent;
+	return withCounter;
 };
 
-export default UpdatedComponent;
+export default withCounter;
