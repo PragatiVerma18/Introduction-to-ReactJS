82697fb173c69fe018f581a6c0ee814c1bcf4aec
pragati <itispragativerma@gmail.com>
2020-06-11 13:16:37 +0530

    useEffect with cleanup

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HookMouse from './Components/HookMouse';
+import MouseContainer from './Components/MouseContainer';
+import IntervalHookCounter from './Components/IntervalHookCounter';
 
 function App() {
 	return (
 		<div className='App'>
-			<HookMouse />
+			<IntervalHookCounter />
 		</div>
 	);
 }
