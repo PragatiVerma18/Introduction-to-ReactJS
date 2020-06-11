97fe5508cc437c6747370be9e5132aa7653bc0da
pragati <itispragativerma@gmail.com>
2020-06-11 15:00:26 +0530

    Fetchin Data with useEffect

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import HookMouse from './Components/HookMouse';
-import MouseContainer from './Components/MouseContainer';
-import IntervalHookCounter from './Components/IntervalHookCounter';
+import DataFetching from './Components/DataFetching';
 
 function App() {
 	return (
 		<div className='App'>
-			<IntervalHookCounter />
+			<DataFetching />
 		</div>
 	);
 }
