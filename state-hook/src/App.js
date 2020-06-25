277153129d150bd18edad11657e1c73e5589df46
PragatiVerma18 <itispragativerma@gmail.com>
2020-06-25 19:58:56 +0530

    useRef

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -4,12 +4,18 @@ import "./App.css";
 import DataFetching from "./Components/DataFetching";
 import DataFetchingTwo from "./Components/DataFetchingTwo";
 import Counter from "./Components/Counter";
+import FocusInput from "./Components/FocusInput";
+import ClassTimer from "./Components/ClassTimer";
+import HookTimer from "./Components/HookTimer";
 
 function App() {
   return (
     <div className="App">
       {/* <DataFetchingTwo /> */}
-      <Counter />
+      {/* <Counter /> */}
+      {/* <FocusInput /> */}
+      <ClassTimer />
+      <HookTimer />
     </div>
   );
 }
