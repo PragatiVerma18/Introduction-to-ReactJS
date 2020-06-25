ab1819d6cbeae716b05eaec8aea26e7b66607c88
PragatiVerma18 <itispragativerma@gmail.com>
2020-06-25 23:56:11 +0530

    useCounter custom hook

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 import DocTitleOne from "./Components/DocTitleOne";
 import DocTitleTwo from "./Components/DocTitleTwo";
+import HookCounter from "./Components/HookCounter";
 
 function App() {
   return (
     <div className="App">
       {/* <DocTitleOne /> */}
-      <DocTitleTwo />
+      {/* <DocTitleTwo /> */}
+      <HookCounter />
     </div>
   );
 }
