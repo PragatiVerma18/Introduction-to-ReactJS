21d90d3c281a3fa67813fe73d6a0f82ede392028
PragatiVerma18 <itispragativerma@gmail.com>
2020-06-26 00:16:04 +0530

    useInput custom hook

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -4,13 +4,17 @@ import "./App.css";
 import DocTitleOne from "./Components/DocTitleOne";
 import DocTitleTwo from "./Components/DocTitleTwo";
 import HookCounter from "./Components/HookCounter";
+import UserForm from "./Components/UserForm";
+import UserFormTwo from "./Components/UserFormTwo";
 
 function App() {
   return (
     <div className="App">
       {/* <DocTitleOne /> */}
       {/* <DocTitleTwo /> */}
-      <HookCounter />
+      {/* <HookCounter /> */}
+      {/* <UserForm /> */}
+      <UserFormTwo />
     </div>
   );
 }
---
+++ state-hook/src/Components/UserFormTwo.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import useInput from "../Hooks/useInput";
+
+function UserFormTwo() {
+  const [firstName, bindFirstName, resetFirstName] = useInput("");
+  const [lastName, bindLastName, resetLastName] = useInput("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    alert(`Hello ${firstName} ${lastName}`);
+    resetFirstName();
+    resetLastName();
+  };
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label>First Name</label>
+          <input {...bindFirstName} type="text" />
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input {...bindLastName} type="text" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default UserFormTwo;
