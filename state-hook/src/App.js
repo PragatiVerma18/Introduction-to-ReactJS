2aba8dec064060c63a35eed087887885a48c5939
PragatiVerma18 <itispragativerma@gmail.com>
2020-06-25 15:33:56 +0530

    useMemo

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 
 import DataFetching from "./Components/DataFetching";
 import DataFetchingTwo from "./Components/DataFetchingTwo";
+import Counter from "./Components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <DataFetchingTwo />
+      {/* <DataFetchingTwo /> */}
+      <Counter />
     </div>
   );
 }
